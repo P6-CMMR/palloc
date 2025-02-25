a3a1013501f545a742800ba69180f323164f336c
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2025-02-25 14:19:00 +0100

    added basic argument setup

--- include/palloc.hpp
+++ include/palloc.hpp
@@ -3,5 +3,9 @@
 
 #include <argz/argz.hpp>
 #include <iostream>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <string_view>
 
 #endif
--- src/palloc.cpp
+++ src/palloc.cpp
@@ -1,3 +1,20 @@
 #include "palloc.hpp"
 
-int main(int argc, char **argv) { std::cout << "Hello world" << '\n'; }
+int main(int argc, char **argv) {
+    constexpr std::string_view version = "0.0.1";
+    argz::about about{"Palloc", version};
+
+    std::optional<std::string> environmentPath;
+    argz::options opts{{{"environment", 'e'}, environmentPath, "the environment file to simulate"}};
+
+    try {
+        argz::parse(about, opts, argc, argv);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
+    if (!environmentPath.has_value()) { 
+        return EXIT_SUCCESS;
+    }
+}
