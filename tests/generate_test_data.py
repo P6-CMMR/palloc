74ec73bda10eae4502dac4b0474a61b2e69c5b84
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2025-04-11 12:07:06 +0200

    updated build workflow

--- tests/generate_test_data.py
+++ tests/generate_test_data.py
@@ -2,12 +2,12 @@ import json
  
 # Data to be written
 dictionary = {
-"dropoff_to_parking": [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
-"parking_to_dropoff": [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
-"parking_capacities": [3, 5, 9], 
-"dropoff_coords": [{"latitude": 11, "longitude": 12}, {"latitude": 14, "longitude": 15}, {"latitude": 16, "longitude": 17}], 
-"parking_coords": [{"latitude": 56, "longitude": 57}, {"latitude": 58, "longitude": 59}, {"latitude": 60, "longitude": 61}], 
-"smallest_round_trips": [2, 6, 10]
+    "dropoff_to_parking": [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+    "parking_to_dropoff": [[1, 2, 3], [4, 5, 6], [7, 8, 9]], 
+    "parking_capacities": [3, 5, 9], 
+    "dropoff_coords": [{"latitude": 11, "longitude": 12}, {"latitude": 14, "longitude": 15}, {"latitude": 16, "longitude": 17}], 
+    "parking_coords": [{"latitude": 56, "longitude": 57}, {"latitude": 58, "longitude": 59}, {"latitude": 60, "longitude": 61}], 
+    "smallest_round_trips": [2, 6, 10]
 }
  
 # Serializing json
