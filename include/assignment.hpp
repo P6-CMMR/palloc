55bd15b540eab874c5e26cd9393fae844ddf3254
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2025-04-11 11:50:46 +0200

    Reapply "simplified some headers"
    This reverts commit 7ce5e670e2edf73fc51c251858ad8fad2b92d332.

---
+++ include/assignment.hpp
@@ -0,0 +1,45 @@
+#ifndef ASSIGNMENT_HPP
+#define ASSIGNMENT_HPP
+
+#include <vector>
+#include <cstdint>
+
+#include "glaze/glaze.hpp"
+
+#include "environment.hpp"
+
+namespace palloc {
+    class Assignment {
+        public:
+         explicit Assignment() {}
+         explicit Assignment(Coordinate dropoffCoordinate, Coordinate parkingCoordinate,
+                             uint32_t requestDuration, uint32_t routeDuration)
+             : _dropoffCoordinate(dropoffCoordinate),
+               _parkingCoordinate(parkingCoordinate),
+               _requestDuration(requestDuration),
+               _routeDuration(routeDuration) {}
+     
+         uint32_t getRequestDuration() const noexcept;
+     
+        private:
+         friend struct glz::meta<Assignment>;
+     
+         Coordinate _dropoffCoordinate{};
+         Coordinate _parkingCoordinate{};
+     
+         uint32_t _requestDuration{};
+         uint32_t _routeDuration{};
+     };
+     
+     using Assignments = std::vector<Assignment>;
+}
+
+template <>
+struct glz::meta<palloc::Assignment> {
+    using T = palloc::Assignment;
+    static constexpr auto value = glz::object(
+        "dropoff_coordinate", &T::_dropoffCoordinate, "parking_coordinate", &T::_parkingCoordinate,
+        "request_duration", &T::_requestDuration, "route_duration", &T::_routeDuration);
+};
+
+#endif
--- include/request_generator.hpp
+++ include/request_generator.hpp
@@ -4,38 +4,11 @@
 #include <array>
 #include <cstdint>
 #include <random>
-#include <vector>
 
 #include "types.hpp"
+#include "request.hpp"
 
 namespace palloc {
-
-class Request {
-   public:
-    explicit Request(uint32_t dropoffNode, uint32_t requestDuration, uint32_t tillArrival)
-        : _dropoffNode(dropoffNode), _requestDuration(requestDuration), _tillArrival(tillArrival) {}
-
-    uint32_t getDropoffNode() const noexcept;
-    uint32_t getRequestDuration() const noexcept;
-    uint32_t getTimesDropped() const noexcept;
-    uint32_t getArrival() const noexcept;
-
-    void decrementDuration() noexcept;
-    void decrementTillArrival() noexcept;
-    void incrementTimesDropped() noexcept;
-
-    bool isDead() const noexcept;
-    bool isEarly() const noexcept;
-
-   private:
-    uint32_t _dropoffNode;
-    uint32_t _requestDuration;
-    uint32_t _timesDropped = 0;
-    uint32_t _tillArrival;
-};
-
-using Requests = std::vector<Request>;
-
 class RequestGenerator {
    public:
     struct Options {
