22403a460065237fb27786688a3bc29386fdf0f1
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2025-04-25 10:13:19 +0200

    cost update

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -81,7 +81,7 @@ SchedulerResult Scheduler::scheduleBatch(Environment &env, Requests &requests,
             if (useWeightedParking) {
                 const auto &parkingWeights = env.getParkingWeights();
                 assert(parkingWeights[j] >= 0.0 && parkingWeights[j] <= 1.0);
-                cost *= parkingWeights[j];
+                cost *= 1 + parkingWeights[j];
             }
 
             objective += std::lround(cost) * sat::LinearExpr(var[i][j]);
