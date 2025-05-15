f315745b1ca5a252720e0ea9bd966710018a9fd7
MadsVind <madsvind7@gmail.com>
2025-05-15 12:27:19 +0200

    again again

--- analysis/generate_report.py
+++ analysis/generate_report.py
@@ -446,7 +446,7 @@ def add_latex_contour_graph_to(file_path, x, y, z, title, x_title, y_title):
             width=\\columnwidth*0.9,
             view={{0}}{{90}},
             colormap={{CM}}{{
-            samples of colormap=(20 of viridis)}},
+            samples of colormap=(13 of viridis)}},
             colormap access=piecewise constant,
             colorbar horizontal,
             colorbar style={{%
