8130b122916e20aded7deb3f4c21daa4780cd86f
MadsVind <madsvind7@gmail.com>
2025-05-15 09:29:19 +0200

    forgot to escape some brackets upsie daysie

--- analysis/generate_report.py
+++ analysis/generate_report.py
@@ -445,12 +445,12 @@ def add_latex_contour_graph_to(file_path, x, y, z, title, x_title, y_title):
             width=\\columnwidth*0.9,
             view={{0}}{{90}},
             colormap={{CM}}{{
-            samples of colormap=(20 of viridis)]},
+            samples of colormap=(20 of viridis)]}},
             colormap access=piecewise constant,
             colorbar right,
             colorbar style={{%
-            ytick=data,
-            }]
+                ytick=data,
+            }}]
 
         \\addplot3[
             surf,
